da6e71b5c112759513abc7eeca78503acfb10369
ibrahimabdalrhman <ibrahimabdalr7man@gmail.com>
2024-10-04 23:40:45 +0200

    complete chatController by create fetchChats, createGroup,renameGroup, addToGroupremoveUserFromGroup

--- backend/controllers/chatController.js
+++ backend/controllers/chatController.js
@@ -7,7 +7,7 @@ exports.accessChat = asyncHandler(async (req, res, next) => {
   const { userId } = req.body;
 
   if (!userId) {
-    return res.status(400).send({ message: "UserId not provided" });
+    return res.status(400).json({ message: "UserId not provided" });
   }
 
   const user = await User.findById(userId);
@@ -43,15 +43,117 @@ exports.accessChat = asyncHandler(async (req, res, next) => {
   return res.status(200).json({
     chat: fullChat,
   });
-
 });
 
-exports.fetchChats = asyncHandler(async (req, res, next) => {});
+exports.fetchChats = asyncHandler(async (req, res, next) => {
+  const chats = await Chat.find({
+    users: { $elemMatch: { $eq: req.user._id } },
+  })
+    .populate("users", "-password")
+    .populate("latestMessages");
 
-exports.createGroup = asyncHandler(async (req, res, next) => {});
+  res.status(200).json({
+    chats,
+  });
+});
 
-exports.renameGroup = asyncHandler(async (req, res, next) => {});
+exports.createGroup = asyncHandler(async (req, res, next) => {
+  const { groupName, users } = req.body;
+  if (!groupName || !users) {
+    return next(new ApiError("Please fill all the fields", 400));
+  }
+  const validUsers = [];
 
-exports.RemoveGroup = asyncHandler(async (req, res, next) => {});
+  for (const userId of users) {
+    const validUser = await User.findById(userId);
+    if (!validUser) {
+      return next(new ApiError(`User ID ${userId} not found`, 404));
+    }
+    validUsers.push(validUser._id);
+  }
+  const group = await Chat.create({
+    chatName: groupName,
+    isGroup: true,
+    users: validUsers,
+    groupAdmin: req.user._id,
+  });
+  const fullGroupChat = await Chat.findOne({ _id: group._id })
+    .populate("users", "-password")
+    .populate("groupAdmin", "-password");
 
-exports.addToGroup = asyncHandler(async (req, res, next) => {});
+  res.status(200).json(fullGroupChat);
+});
+
+exports.renameGroup = asyncHandler(async (req, res, next) => {
+  const { chatId, chatName } = req.body;
+
+  const updatedChat = await Chat.findByIdAndUpdate(
+    chatId,
+    { chatName },
+    { new: true }
+  )
+    .populate("users", "-password")
+    .populate("groupAdmin", "-password");
+
+  res.status(200).json(updatedChat);
+});
+
+exports.RemoveGroup = asyncHandler(async (req, res) => {
+  const { chatId, userId } = req.body;
+
+  const updatedChat = await Chat.findByIdAndUpdate(
+    chatId,
+    { $pull: { users: userId } },
+    { new: true }
+  ).populate("users", "-password");
+
+  res.status(200).json(updatedChat);
+});
+
+exports.addToGroup = asyncHandler(async (req, res) => {
+  const { chatId, userId } = req.body;
+
+  const validUser = await User.findById(userId);
+  if (!validUser) {
+    return next(new ApiError(`User ID ${userId} not found`, 404));
+  }
+  const updatedChat = await Chat.findByIdAndUpdate(
+    chatId,
+    { $push: { users: userId } },
+    { new: true }
+  ).populate("users", "-password");
+
+  res.status(200).json(updatedChat);
+});
+
+exports.removeUserFromGroup = asyncHandler(async (req, res, next) => {
+  const { chatId, userId } = req.body;
+
+  if (!chatId || !userId) {
+    return next(new ApiError("Chat ID and User ID are required", 400));
+  }
+
+  const chatGroup = await Chat.findById(chatId);
+  if (!chatGroup) {
+    return next(new ApiError("Chat group not found", 404));
+  }
+
+  if (!chatGroup.users.includes(userId)) {
+    return next(new ApiError("User is not in the group", 400));
+  }
+
+  if (chatGroup.groupAdmin.toString() !== req.user._id.toString()) {
+    return next(new ApiError("Only the group admin can remove users", 403));
+  }
+
+  chatGroup.users = chatGroup.users.filter(
+    (user) => user.toString() !== userId
+  );
+  await chatGroup.save();
+
+  const updatedGroupChat = await Chat.findById(chatId)
+    .populate("users", "-password")
+    .populate("groupAdmin", "-password");
+
+  res.status(200).json(updatedGroupChat);
+});
--- backend/routes/chatRoute.js
+++ backend/routes/chatRoute.js
@@ -9,7 +9,8 @@ router.get('/',authMiddleware,chatController.fetchChats)
 router.post('/group',authMiddleware,chatController.createGroup)
 router.post('/rename',authMiddleware,chatController.renameGroup)
 router.post('/groupRemove',authMiddleware,chatController.RemoveGroup)
-router.post('/groupAdd',authMiddleware,chatController.addToGroup)
+router.post('/addUserToGroup',authMiddleware,chatController.addToGroup)
+router.post('/RemoveUserFromGroup',authMiddleware,chatController.removeUserFromGroup)
 
 
 
